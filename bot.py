5c84744b47e0b42c4a6ff64e83656e5e47038ef8
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-02-13 15:46:40 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -78,7 +78,7 @@ async def github():
 @bot.command(pass_context=True)
 async def roles(ctx):
     """Give the list of roles with their IDs"""
-    roles = ctx.author.server.roles
+    roles = ctx.server.roles
     print(roles)
     
 
