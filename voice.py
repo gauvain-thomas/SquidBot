6f650e516f6b694a9adb90a2fdadd1d0ebf531a6
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-02-14 22:34:32 +0100

    Update voice.py

--- voice.py
+++ voice.py
@@ -2,3 +2,9 @@ import discord
 from discord.ext import commands
 
 class Voice:
+  def __init__(self, client):
+    self.client = client
+    
+  @commands.command()
+  async def ping(self):
+    await self.client.say('Pong')
