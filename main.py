ee76b9413aabd57fb67a32407fa44051d04b28eb
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-02-15 21:09:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -32,9 +32,6 @@ async def on_ready():
     print('------')
     await client.change_presence(game=discord.Game(name='humans...', type=3))
     
-    user = client.get_member('263657174230761483')
-    await client.send_message(user, "Bonjour")
-    
 @client.event
 async def on_member_join(member):
     await client.send_message(member, "Hello")
