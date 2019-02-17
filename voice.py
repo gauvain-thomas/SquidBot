db6a191646bc521ac94198ad5c19d300735bb1e0
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-02-17 21:11:08 +0100

    Update voice.py

--- voice.py
+++ voice.py
@@ -11,7 +11,7 @@ class Voice:
   #Help voice
   @commands.command(pass_context=True)
   async def help(self, ctx):
-    if "all" or "voice" in ctx.message.content:
+    if "all" in ctx.message.content:
       embed=discord.Embed(title="Help - [Voice]", description="You must be in a voice channel to use these commands", color=0x0000ff)
       embed.add_field(name="--------------------", value="--------------------", inline=False)
       embed.add_field(name=".join", value="Join a voice channel", inline=False)
