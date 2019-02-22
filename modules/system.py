b1ba5a945f4556d38c4fb3f05862748d458e8317
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-02-22 22:55:54 +0100

    Update system.py

--- modules/system.py
+++ modules/system.py
@@ -4,4 +4,30 @@
 import discord
 from discord.ext import commands
 import subprocess
-import os
+
+owners = ['263670024391229440']
+
+class System:
+  def __init__(self, client):
+    self.client = client
+
+  @commands.command(pass_context=True)
+  async def reboot(self, ctx):
+      """Reboot client"""
+      if ctx.message.author.id in owners:
+          await self.client.say("Reboot in process...")
+          print("Reboot in process")
+          subprocess.call("../start.sh", shell=True)
+          sys.exit()
+      else:
+          await self.client.say("Access denied")
+
+  @commands.command(pass_context=True)
+  async def shutdown(self, ctx):
+      """Turn client off"""
+      if ctx.message.author.id owners:
+          await self.client.say("Turning off...")
+          print("Turning off...")
+          sys.exit()
+      else:
+          await self.client.say("Access denied")
