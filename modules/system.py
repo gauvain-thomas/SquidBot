3790ffb07faa6ab830c66c3d476dc8b63d944ff9
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-02-22 23:01:44 +0100

    Update system.py

--- modules/system.py
+++ modules/system.py
@@ -25,7 +25,7 @@ class System:
   @commands.command(pass_context=True)
   async def shutdown(self, ctx):
       """Turn client off"""
-      if ctx.message.author.id owners:
+      if ctx.message.author.id in owners:
           await self.client.say("Turning off...")
           print("Turning off...")
           sys.exit()
