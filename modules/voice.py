964d4299f5ab1ffde206868ea0cc04959e8d2a0e
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-02-22 21:15:54 +0100

    Update voice.py

--- modules/voice.py
+++ modules/voice.py
@@ -13,14 +13,14 @@ class Voice:
     print(message)
     
   #Help voice
-embed=discord.Embed(title="Help - [Voice]", description="You must be in a voice channel to use these commands", color=0x0000ff)
-embed.add_field(name="--------------------", value="--------------------", inline=False)
-embed.add_field(name=".join", value="Join a voice channel", inline=False)
-embed.add_field(name=".leave", value="Leave a voice channel", inline=False)
-embed.add_field(name=".play [url]", value="Play audio from the youtube url", inline=False)
-embed.add_field(name=".pause", value="Pause current music", inline=False)
-embed.add_field(name=".resume", value="Resume current music", inline=False)
-embed.add_field(name=".stop", value="stop current music", inline=False)   
+# embed=discord.Embed(title="Help - [Voice]", description="You must be in a voice channel to use these commands", color=0x0000ff)
+# embed.add_field(name="--------------------", value="--------------------", inline=False)
+# embed.add_field(name=".join", value="Join a voice channel", inline=False)
+# embed.add_field(name=".leave", value="Leave a voice channel", inline=False)
+# embed.add_field(name=".play [url]", value="Play audio from the youtube url", inline=False)
+# embed.add_field(name=".pause", value="Pause current music", inline=False)
+# embed.add_field(name=".resume", value="Resume current music", inline=False)
+# embed.add_field(name=".stop", value="stop current music", inline=False)   
 
     
     
