5d948fa8b92b2fb770feb1296f3a09a387b96051
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-02-22 23:07:04 +0100

    Update system.py

--- modules/system.py
+++ modules/system.py
@@ -11,9 +11,20 @@ class System:
   def __init__(self, client):
     self.client = client
 
+  #Help system
+  async def on_message(self, message):
+    if '.help all' in message.content or '.help voice' in message.content:
+      embed=discord.Embed(title="Help - [Systeù]", description="Only authorized members can use these commands", color='red')
+      embed.add_field(name="--------------------", value="--------------------", inline=False)
+      embed.add_field(name=".reboot", value="Restart bot and update code from github", inline=False)
+      embed.add_field(name=".shutdown", value="Turn bot off", inline=False)
+      
+      await self.client.send_message(message.channel, embed=embed)
+      
+  #Commands
   @commands.command(pass_context=True)
   async def reboot(self, ctx):
-      """Reboot client"""
+      """Reboot bot"""
       if ctx.message.author.id in owners:
           await self.client.say("Reboot in process...")
           print("Reboot in process")
@@ -24,7 +35,7 @@ class System:
 
   @commands.command(pass_context=True)
   async def shutdown(self, ctx):
-      """Turn client off"""
+      """Turn bot off"""
       if ctx.message.author.id in owners:
           await self.client.say("Turning off...")
           print("Turning off...")
