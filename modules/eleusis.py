fbc9c88c802861585d98935607b0a29c937e23a5
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-03-01 23:11:58 +0100

    Added rules command

--- modules/eleusis.py
+++ modules/eleusis.py
@@ -15,38 +15,19 @@ class System:
 
   #Help system
   async def on_message(self, message):
-    if '.help all' in message.content or '.help system' in message.content:
-      embed=discord.Embed(title="Help - [System]", description="Only authorized members can use these commands", color=0xff0000)
+    if '.help all' in message.content or '.help eulesis' in message.content:
+      embed=discord.Embed(title="Help - [System]", description=" - ", color=0x00ffff)
       embed.add_field(name="--------------------", value="--------------------", inline=False)
-      embed.add_field(name=".reboot", value="Restart bot and update code from github", inline=False)
-      embed.add_field(name=".shutdown", value="Turn bot off", inline=False)
       
       await self.client.send_message(message.channel, embed=embed)
       
   #Commands
   @commands.command(pass_context=True)
-  async def reboot(self, ctx):
-      """Reboot bot"""
-      if ctx.message.author.id in owners:
-        await self.client.say("Reboot in process...")
-        print("Reboot in process")
-        source = os.path.dirname(__file__)
-        parent = os.path.join(source, '../')
-        script_path = os.path.join(parent, 'start.sh')
-        subprocess.call(script_path, shell=True)
-        sys.exit()
-      else:
-          await self.client.say("Access denied")
+  async def eleusis(self, ctx, args*):
+      """Eleusis commands"""
+      if args[0] == 'rules':
+        await self.clinet.say('http://laelith.fr/Zet/Articles/images/eleusis.pdf')
 
-  @commands.command(pass_context=True)
-  async def shutdown(self, ctx):
-      """Turn bot off"""
-      if ctx.message.author.id in owners:
-          await self.client.say("Turning off...")
-          print("Turning off...")
-          sys.exit()
-      else:
-          await self.client.say("Access denied")
-
+          
 def setup(client):
   client.add_cog(System(client))
