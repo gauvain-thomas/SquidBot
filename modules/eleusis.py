066da66bbcef371af5d771fd7952ccdfd17e45c5
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-03-01 23:23:39 +0100

    Update eleusis.py

--- modules/eleusis.py
+++ modules/eleusis.py
@@ -15,7 +15,7 @@ class Eleusis:
 
   #Help system
   async def on_message(self, message):
-    if '.help all' in message.content or '.help eulesis' in message.content:
+    if '.help all' in message.content or '.help eleusis' in message.content:
       embed=discord.Embed(title="Help - [Eleusis]", description="Commands about Eleusis", color=0x00ffff)
       embed.add_field(name="--------------------", value="--------------------", inline=False)
       embed.add_field(name=".eleusis rules", value="Give link to the game's rules", inline=False)
