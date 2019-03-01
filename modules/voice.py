0eeb13f15c39e641cf540966ac1d296e2e79203a
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-03-01 23:56:04 +0100

    Added youtube search

--- modules/voice.py
+++ modules/voice.py
@@ -1,6 +1,65 @@
 import discord
 from discord.ext import commands
 import youtube_dl
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+
+
+DEVELOPER_KEY = "AIzaSyC7Zxm1fWNZJOiw_CNIuB5s99x9DxKz0ao"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+
+def youtube_search(q, max_results=50,order="relevance", token=None, location=None, location_radius=None):
+
+  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+    developerKey=DEVELOPER_KEY)
+
+  search_response = youtube.search().list(
+    q=q,
+    type="video",
+    pageToken=token,
+    order = order,
+    part="id,snippet",
+    maxResults=max_results,
+    location=location,
+    locationRadius=location_radius
+
+  ).execute()
+
+
+
+  videos = []
+
+  for search_result in search_response.get("items", []):
+    if search_result["id"]["kind"] == "youtube#video":
+      videos.append(search_result)
+  try:
+      nexttok = search_response["nextPageToken"]
+      return(nexttok, videos)
+  except Exception as e:
+      nexttok = "last_page"
+      return(nexttok, videos)
+
+
+def geo_query(video_id):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                    developerKey=DEVELOPER_KEY)
+
+    video_response = youtube.videos().list(
+        id=video_id,
+        part='snippet, recordingDetails, statistics'
+
+    ).execute()
+
+    return video_response
+
+
+
+
+
+
+
 
 players = {}
 
@@ -68,6 +127,12 @@ class Voice:
     id = ctx.message.server.id
     players[id].stop()
     await self.client.say(":stop_button: Music stopped :stop_button:")
+    
+    
+  @commands.command(pass_context=True)
+  async def youtube(self, ctx, arg):
+    search = youtube_search(arg)
+    await self.client.say(search)
 
 
 def setup(client):
