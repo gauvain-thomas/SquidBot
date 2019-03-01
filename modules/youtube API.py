90c909770825e43658ff0e55a3375ef2f9ab1a13
LeoLeLion <46904198+LeoLeLion@users.noreply.github.com>
2019-03-01 23:39:48 +0100

    Update youtube API.py

--- modules/youtube API.py
+++ modules/youtube API.py
@@ -1,57 +1,43 @@
-#!/usr/bin/python
-
-from apiclient.discovery import build
-from apiclient.errors import HttpError
-from oauth2client.tools import argparser
-
-
-# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
-# tab of
-#   https://cloud.google.com/console
-# Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "REPLACE_ME"
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
-
-def youtube_search(options):
-  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-    developerKey=DEVELOPER_KEY)
-
-  # Call the search.list method to retrieve results matching the specified
-  # query term.
-  search_response = youtube.search().list(
-    q=options.q,
-    part="id,snippet",
-    maxResults=options.max_results
-  ).execute()
-  videos = []
-  channels = []
-  playlists = []
-
-  # Add each result to the appropriate list, and then display the lists of
-  # matching videos, channels, and playlists.
-  for search_result in search_response.get("items", []):
-    if search_result["id"]["kind"] == "youtube#video":
-      videos.append("%s (%s)" % (search_result["snippet"]["title"],
-                                 search_result["id"]["videoId"]))
-    elif search_result["id"]["kind"] == "youtube#channel":
-      channels.append("%s (%s)" % (search_result["snippet"]["title"],
-                                   search_result["id"]["channelId"]))
-    elif search_result["id"]["kind"] == "youtube#playlist":
-      playlists.append("%s (%s)" % (search_result["snippet"]["title"],
-                                    search_result["id"]["playlistId"]))
-
-  print "Videos:\n", "\n".join(videos), "\n"
-  print "Channels:\n", "\n".join(channels), "\n"
-  print "Playlists:\n", "\n".join(playlists), "\n"
+import sys
+sys.path.append('SquidBot/modules/')
+from youtube_videos import youtube_search
+import json
 
 
-if __name__ == "__main__":
-  argparser.add_argument("--q", help="Search term", default="Google")
-  argparser.add_argument("--max-results", help="Max results", default=25)
-  args = argparser.parse_args()
+test = youtube_search("spinners")
 
-  try:
-    youtube_search(args)
-  except HttpError, e:
-    print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
+video_dict = {'youID':[], 'title':[], 'pub_date':[]}
+
+just_json = test[1]
+len(just_json)
+
+
+for video in just_json:
+    print (video['snippet']['title'])
+
+token = test[0]
+youtube_search("spinners", token=token)
+
+
+
+video_dict = {'youID':[], 'title':[], 'pub_date':[]}
+
+def grab_videos(keyword, token=None):
+    res = youtube_search(keyword, token=token)
+    token = res[0]
+    videos = res[1]
+    for vid in videos:
+        video_dict['youID'].append(vid['id']['videoId'])
+        video_dict['title'].append(vid['snippet']['title'])
+        video_dict['pub_date'].append(vid['snippet']['publishedAt'])
+    print ("added " + str(len(videos)) + " videos to a total of " + str(len(video_dict['youID'])))
+    return token
+
+
+
+
+
+token = grab_videos("spinners")
+while token != "last_page":
+    token = grab_videos("spinners", token=token)
+
