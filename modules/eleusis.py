71346f257635e5217ea607038cb9b383baabb49b
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-03-02 23:50:45 +0100

    Update eleusis.py

--- modules/eleusis.py
+++ modules/eleusis.py
@@ -23,7 +23,7 @@ class Eleusis:
       embed.add_field(name=".eleusis start", value="Launch a new game, must be used after creating a new party", inline=False)
       await self.client.send_message(message.channel, embed=embed)
       
-      game = Game()
+      game = Game(self.client)
       
       
   #Commands
@@ -75,7 +75,8 @@ def setup(client):
   
   
 class Game:
-  def __init__(self):
+  def __init__(self, client):
+    self.client = client
     print('test')
 #     self.client=client
 #     self.channel = channel
