fe0deef36bc6ecf4a3f8b29bec1dd90574885ee0
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-03-02 21:03:24 +0100

    Update eleusis.py

--- modules/eleusis.py
+++ modules/eleusis.py
@@ -38,12 +38,11 @@ class Eleusis:
         await self.client.say('http://laelith.fr/Zet/Articles/images/eleusis.pdf')
         
       elif args[0] == 'create':
-        poll_msg = await self.client.say("**Poll :**")
-        
-        end_msg = await self.client.wait_for_message(author=ctx.message.author, content=".poll stop")
-        poll_msg = await self.client.get_message(ctx.message.channel, poll_msg.id)
+        new_game__msg = await self.client.say("**A new game of Eleusis is being created ! React to join the party !**")
+        start_game__msg = await self.client.wait_for_message(author=ctx.message.author, content=".eleusis stop")
+        new_game_msg = await self.client.get_message(ctx.message.channel, new_game_msg.id)
 
-        for reaction in poll_msg.reactions:
+        for reaction in new_game_msg.reactions:
           await self.client.say(reaction)
           await self.client.say(reaction.emoji)
 #         new_game_msg = await self.client.say('A new game of Eleusis is being created ! React to join the party !')
