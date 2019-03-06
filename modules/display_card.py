a49fa83b6d72d84ccb73a375362260bf55450acc
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-03-06 18:00:25 +0100

    Uncomment

--- modules/display_card.py
+++ modules/display_card.py
@@ -8,15 +8,15 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]),'modules/cards_img'))
 print(sys.path)
 
-# cardSize = ( 50 , 67 )
-# h_gap = 40
-# v_gap1 = 24
-# v_gap2 = 25
-#
-# row = 1
-# column = 2
-#
-# img = Image.open("cards_img.plateau.png")
+cardSize = ( 50 , 67 )
+h_gap = 40
+v_gap1 = 24
+v_gap2 = 25
+
+row = 1
+column = 2
+
+img = Image.open("cards_img.plateau.png")
 # card = Image.open("1.jpg")
 #
 # position1 = (column*(h_gap+cardSize[0])+h_gap , v_gap1+row*(v_gap2+cardSize[1]) )
