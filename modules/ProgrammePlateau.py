a85adf39102a5aa41d03260b53bebeebbfc2dfe9
LeoLeLion <46904198+LeoLeLion@users.noreply.github.com>
2019-03-06 17:14:14 +0100

    Add files via upload

---
+++ modules/ProgrammePlateau.py
@@ -0,0 +1,28 @@
+from PIL import Image
+import io
+import pathlib
+import json
+import os
+
+cardSize = ( 50 , 67 )
+h_gap = 40
+v_gap1 = 24
+v_gap2 = 25
+
+row = 1
+column = 2
+
+img = Image.open("plateau.png")
+card = Image.open("1.jpg")
+
+position1 = (column*(h_gap+cardSize[0])+h_gap , v_gap1+row*(v_gap2+cardSize[1]) )
+position2 = (position1[0]+cardSize[0], position1[1]+cardSize[1])
+print (position1)
+print (position2)
+
+img.paste(card, position1+position2)
+
+img.show()
+img.close()
+print("Everything is okay")
+os.system("pause")
