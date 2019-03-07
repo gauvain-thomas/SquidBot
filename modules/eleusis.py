1bf732a82bd85393327057aa620eb3edf929a799
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-03-07 22:29:12 +0100

    Fix

--- modules/eleusis.py
+++ modules/eleusis.py
@@ -115,8 +115,8 @@ class Game:
                     chosen_card_msg = await self.client.wait_for_message(author=player)
                     chosen_card = chosen_card_msg.content
 
-                await self.client.send_message(god, 'Does this card fit the sequence ? : {}'.format(chosen_card))
-                answer = await self.client.wait_for_message(author=god)
+                await self.client.send_message(self.god, 'Does this card fit the sequence ? : {}'.format(chosen_card))
+                answer = await self.client.wait_for_message(author=self.god)
 
                 while answer.content is 'yes' or answer.content is 'no':
                     if answer.content is 'yes':
@@ -125,7 +125,7 @@ class Game:
                     elif answer.content is 'no':
                         self.down_row.append((turn, chosen_card))
                     else:
-                        await self.client.send_message(god,
+                        await self.client.send_message(self.god,
                         'Sorry, your message was not fully understood, please try again')
 
                     self.show_cards()
