94ae451b73d1160c6d8d1760bd3937fae3eb54b5
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-03-07 21:55:59 +0100

    Fix

--- modules/eleusis.py
+++ modules/eleusis.py
@@ -109,7 +109,7 @@ class Game:
         for player in self.players:
             if not self.players_obj[player.id].is_god:
                 card = ''
-                while chosen_card is not in self.cards:
+                while not chosen_card in self.cards:
                     await self.client.send_message(player, 'Choose a card.. .')
                     chosen_card = await self.client.wait_for_message(author=player)
 
