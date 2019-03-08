201fa1ab5091b58386afdc1b0f9cf02ceedbd93e
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-03-08 22:14:43 +0100

    Fix

--- modules/eleusis.py
+++ modules/eleusis.py
@@ -90,9 +90,9 @@ class Game:
 
     async def show_cards(self):
         display_cards.create_image(self.up_row, self.middle_row, self.down_row)
-        await bot.send_file(self.channel, r"~/Bot/SquidBot/modules/cards_img/temp",filename="Cards",content='Cards')
-        await bot send_message(self.channel, 'Cards are : {}'.format(self.middle_row))
-        await bot send_message(self.channel, 'Rejected cards are : {}'.format(self.down_row))
+        await self.client.send_file(self.channel, r"~/Bot/SquidBot/modules/cards_img/temp",filename="Cards",content='Cards')
+        await self.client.send_message(self.channel, 'Cards are : {}'.format(self.middle_row))
+        await self.client.send_message(self.channel, 'Rejected cards are : {}'.format(self.down_row))
 
     async def show_decks(self):
         for player in self.players:
