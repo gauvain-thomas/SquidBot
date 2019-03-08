d89d0c1063e35a6786a1c1edf33aebf5232f5a00
LeoLeLion <leoelbartodu69lol@outlook.fr>
2019-03-08 21:38:47 +0100

    Fix display_cards and create temp image

--- modules/cards_img/display_cards.py
+++ modules/cards_img/display_cards.py
@@ -1,15 +1,5 @@
 from PIL import Image
-import io
-import pathlib
-import json
 import os
-# import sys
-
-# sys.path.append(os.path.join(os.path.dirname(sys.path[0]),'modules/cards_img'))
-# print(sys.path)
-
-#return the position in the image of the card, given a row and a column
-
 
 def get_folder():
     return os.path.dirname(__file__)
@@ -30,7 +20,7 @@ def get_position(row, column, card_index = None, cards_number = None):
     horizontal_gap = 40
     vertical_gap1 = 24
     vertical_gap2 = 25
-    cards_disparity = 30
+    cards_disparity = 35
 
     if card_index == None or cards_number == None:
 
@@ -40,7 +30,7 @@ def get_position(row, column, card_index = None, cards_number = None):
 
     else:
 
-        position1 = (int(column*(horizontal_gap+card_width)  + horizontal_gap + ((cards_number - card_index - 1)* 30 / (cards_number - 1))), row*(vertical_gap2+card_height) + vertical_gap1)
+        position1 = (int(column*(horizontal_gap+card_width)  + horizontal_gap + ((cards_number - card_index - 1)*cards_disparity / (cards_number - 1))), row*(vertical_gap2+card_height) + vertical_gap1)
         position2 = (position1[0] + card_width, position1[1] + card_height)
 
     print(position1+position2)
@@ -78,9 +68,8 @@ def create_image(up_row, middle_row, down_row):
     print('appelle de paste_down_row:\n')
     stack = paste_border_row(down_row, 2, current_round, stack)
     stack = paste_border_row(up_row, 0, current_round, stack)
-    stack.show()
-    print(current_round)
-    print(middle_row)
+    stack.save(get_folder() + "\\temp.png")
+
 
 def paste_middle_row(middle_row, current_round, stack):
 
@@ -109,7 +98,7 @@ def paste_border_row(row_list, row_number, current_round, stack):
         card_tuple = row_list[index]
         card_round = card_tuple[0]
 
-        if card_tuple[0] < stop_round:
+        if card_tuple[0] <= stop_round:
 
             break
 
@@ -125,18 +114,30 @@ def paste_border_row(row_list, row_number, current_round, stack):
 
         print(index)
         print(previous_index)
+        cards_number = previous_index - index
+        print('cards_number')
+        print(cards_number)
         column = 3 - (current_round - card_round)
-        for x in range(previous_index, index, -1):
-
-
-            card_tuple = row_list[x]
+        if cards_number == 1:
+            card_tuple = row_list[previous_index]
             stack = paste_card(card_tuple, get_position(row_number, column), stack)
+
+        else:
+            for x in range(previous_index, index, -1):
+
+
+                card_tuple = row_list[x]
+
+                card_index = -(x - previous_index)
+                print('card_index')
+                print(card_index)
+                stack = paste_card(card_tuple, get_position(row_number, column, card_index, cards_number), stack)
 
     return stack
 
 
 if __name__ == '__main__':
-    up_row = [(1, '3_Spades'),  (1, '5_Hearts'),  (3, '1_Spades'), (5, 'K_Diamonds')]
+    up_row = [(4, '3_Spades'),  (4, '5_Hearts'),  (4, '1_Spades'), (4, 'K_Diamonds'),(4, '3_Spades'),  (4, '5_Hearts'),  (4, '1_Spades'), (4, 'K_Diamonds')]
     middle_row = [(1, '3_Spades'),  (2, '5_Hearts'),  (3, '1_Spades'), (4, 'K_Diamonds')]
     down_row = [(3, '3_Spades'),  (4, '4_Hearts'),  (5, '8_Spades'), (5, 'K_Diamonds')]
 
