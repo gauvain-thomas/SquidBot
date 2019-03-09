b236cbe20d461821e0394915f21da580124b4d7e
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-03-09 21:59:29 +0100

    Fix

--- modules/eleusis.py
+++ modules/eleusis.py
@@ -85,8 +85,8 @@ class Game:
 
         self.middle_row.append((self.turn, random.choice(Game.cards)))
 
-        await self.show_cards()
-        # await self.show_decks()
+        # await self.show_cards()
+        await self.show_decks()
 
         await self.process_turn()
 
