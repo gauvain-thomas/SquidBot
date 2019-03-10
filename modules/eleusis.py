5c2487784d2454eefa1a9308bdad6468a578bd32
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-03-10 16:30:22 +0100

    Typo

--- modules/eleusis.py
+++ modules/eleusis.py
@@ -144,7 +144,7 @@ class Game:
                     chosen_card = chosen_card_msg.content
 
                     if chosen_card.isdigit():
-                        if int(chosen_card) > 0 and int(chosen_card) <= len(self.players_obj[player.id].deck):
+                        if int(chosen_card) >= 0 and int(chosen_card) <= len(self.players_obj[player.id].deck):
                             chosen_card = self.players_obj[player.id].deck[int(chosen_card)]
                         else:
                             await self.client.send_message(player, 'Index out of range, please try again')
