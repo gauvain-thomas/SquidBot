89000abab861c040bd8c2f39c6132d7c1b59356e
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-03-10 17:38:53 +0100

    Uncomment test

--- modules/voice.py
+++ modules/voice.py
@@ -1,58 +1,58 @@
 import discord
 from discord.ext import commands
 import youtube_dl
-# from apiclient.discovery import build
-# from apiclient.errors import HttpError
+from apiclient.discovery import build
+from apiclient.errors import HttpError
 
 
-# DEVELOPER_KEY = "AIzaSyC7Zxm1fWNZJOiw_CNIuB5s99x9DxKz0ao"
-# YOUTUBE_API_SERVICE_NAME = "youtube"
-# YOUTUBE_API_VERSION = "v3"
+DEVELOPER_KEY = "AIzaSyC7Zxm1fWNZJOiw_CNIuB5s99x9DxKz0ao"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
 
 
-# def youtube_search(q, max_results=50,order="relevance", token=None, location=None, location_radius=None):
+def youtube_search(q, max_results=50,order="relevance", token=None, location=None, location_radius=None):
 
-#   youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-#     developerKey=DEVELOPER_KEY)
+  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+    developerKey=DEVELOPER_KEY)
 
-#   search_response = youtube.search().list(
-#     q=q,
-#     type="video",
-#     pageToken=token,
-#     order = order,
-#     part="id,snippet",
-#     maxResults=max_results,
-#     location=location,
-#     locationRadius=location_radius
+  search_response = youtube.search().list(
+    q=q,
+    type="video",
+    pageToken=token,
+    order = order,
+    part="id,snippet",
+    maxResults=max_results,
+    location=location,
+    locationRadius=location_radius
 
-#   ).execute()
-
-
-
-#   videos = []
-
-#   for search_result in search_response.get("items", []):
-#     if search_result["id"]["kind"] == "youtube#video":
-#       videos.append(search_result)
-#   try:
-#       nexttok = search_response["nextPageToken"]
-#       return(nexttok, videos)
-#   except Exception as e:
-#       nexttok = "last_page"
-#       return(nexttok, videos)
+  ).execute()
 
 
-# def geo_query(video_id):
-#     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-#                     developerKey=DEVELOPER_KEY)
 
-#     video_response = youtube.videos().list(
-#         id=video_id,
-#         part='snippet, recordingDetails, statistics'
+  videos = []
 
-#     ).execute()
+  for search_result in search_response.get("items", []):
+    if search_result["id"]["kind"] == "youtube#video":
+      videos.append(search_result)
+  try:
+      nexttok = search_response["nextPageToken"]
+      return(nexttok, videos)
+  except Exception as e:
+      nexttok = "last_page"
+      return(nexttok, videos)
 
-#     return video_response
+
+def geo_query(video_id):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                    developerKey=DEVELOPER_KEY)
+
+    video_response = youtube.videos().list(
+        id=video_id,
+        part='snippet, recordingDetails, statistics'
+
+    ).execute()
+
+    return video_response
 
 
 
@@ -66,7 +66,7 @@ players = {}
 class Voice:
   def __init__(self, client):
     self.client = client
-    
+
   #Help voice
   async def on_message(self, message):
     if '.help all' in message.content or '.help voice' in message.content:
@@ -78,15 +78,15 @@ class Voice:
       embed.add_field(name=".pause", value="Pause current music", inline=False)
       embed.add_field(name=".resume", value="Resume current music", inline=False)
       embed.add_field(name=".stop", value="stop current music", inline=False)
-      
+
       await self.client.send_message(message.channel, embed=embed)
-    
 
 
-    
-    
+
+
+
   #Voice commands
-  
+
   @commands.command(pass_context=True)
   async def join(self, ctx):
     channel = ctx.message.author.voice.voice_channel
@@ -100,7 +100,7 @@ class Voice:
     voice_client = self.client.voice_client_in(server)
     await voice_client.disconnect()
     await self.client.say(":microphone: Left '{}' voice channel :microphone:".format(channel.name))
-    
+
   @commands.command(pass_context=True)
   async def play(self, ctx, url):
     server = ctx.message.server
@@ -109,30 +109,30 @@ class Voice:
     players[server.id] = player
     player.start()
     await self.client.say(":musical_note: Now playing : {} :musical_note:".format(url))
-    
+
   @commands.command(pass_context=True)
   async def pause(self, ctx):
     id = ctx.message.server.id
     players[id].pause()
     await self.client.say(":pause_button: Music paused :pause_button:")
-    
+
   @commands.command(pass_context=True)
   async def resume(self, ctx):
     id = ctx.message.server.id
     players[id].resume()
     await self.client.say(":play_pause: Music resumed :play_pause:")
-    
+
   @commands.command(pass_context=True)
   async def stop(self, ctx):
     id = ctx.message.server.id
     players[id].stop()
     await self.client.say(":stop_button: Music stopped :stop_button:")
 
-    
-#   @commands.command(pass_context=True)
-#   async def youtube(self, ctx, arg):
-#     search = youtube_search(arg)
-#     await self.client.say(search)
+
+  @commands.command(pass_context=True)
+  async def youtube(self, ctx, arg):
+    search = youtube_search(arg)
+    await self.client.say(search)
 
 
 def setup(client):
