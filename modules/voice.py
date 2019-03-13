d179f13cb4d0a53ca920925104803e742af5e197
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-03-13 18:52:42 +0100

    Comment librairies

--- modules/voice.py
+++ modules/voice.py
@@ -4,8 +4,8 @@ import youtube_dl
 import json
 # from apiclient.discovery import build
 # from apiclient.errors import HttpError
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+# from googleapiclient.discovery import build
+# from googleapiclient.errors import HttpError
 DEVELOPER_KEY = "AIzaSyC7Zxm1fWNZJOiw_CNIuB5s99x9DxKz0ao"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
