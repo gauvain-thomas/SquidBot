25c332ada3a0e220a6642d86a15ea3844fc6fbef
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-03-14 21:22:50 +0100

    Error module fix

--- modules/errors.py
+++ modules/errors.py
@@ -13,33 +13,32 @@ class CommandErrorHandler:
         ctx   : Context
         error : Exception"""
 
-        await self.client.send_message(ctx.message.channel, traceback.format_exc())
-        ctx.send('test')
+        ctx.send(traceback.format_exc())
 
-        # if hasattr(ctx.command, 'on_error'):
-        #     return
-        #
-        # ignored = (commands.CommandNotFound, commands.UserInputError)
-        # error = getattr(error, 'original', error)
-        #
-        # if isinstance(error, ignored):
-        #     return
-        #
-        # elif isinstance(error, commands.DisabledCommand):
-        #     return await ctx.send(f'{ctx.command} has been disabled.')
-        #
-        # elif isinstance(error, commands.NoPrivateMessage):
-        #     try:
-        #         return await ctx.author.send(f'{ctx.command} can not be used in Private Messages.')
-        #     except:
-        #         pass
-        #
-        # elif isinstance(error, commands.BadArgument):
-        #     if ctx.command.qualified_name == 'tag list':
-        #         return await ctx.send('I could not find that member. Please try again.')
-        #
-        # print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-        # traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+        if hasattr(ctx.command, 'on_error'):
+            return
+
+        ignored = (commands.CommandNotFound, commands.UserInputError)
+        error = getattr(error, 'original', error)
+
+        if isinstance(error, ignored):
+            return
+
+        elif isinstance(error, commands.DisabledCommand):
+            return await ctx.send(f'{ctx.command} has been disabled.')
+
+        elif isinstance(error, commands.NoPrivateMessage):
+            try:
+                return await ctx.author.send(f'{ctx.command} can not be used in Private Messages.')
+            except:
+                pass
+
+        elif isinstance(error, commands.BadArgument):
+            if ctx.command.qualified_name == 'tag list':
+                return await ctx.send('I could not find that member. Please try again.')
+
+        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 
 def setup(client):
     client.add_cog(CommandErrorHandler(client))
