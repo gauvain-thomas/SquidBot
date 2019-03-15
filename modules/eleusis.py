8e009c614c66afbc0ed8d1a09a1b8291dd1eff1e
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-03-15 21:41:35 +0100

    Test reaction

--- modules/eleusis.py
+++ modules/eleusis.py
@@ -201,6 +201,9 @@ class Game:
 
                 while not (answer_message == 'yes' or answer_message == 'no'):
                     answer_message = answer.content
+
+                    self.client.add_reaction(answer, '☑')
+                    self.client.add_reaction(answer, '❎')
                     if answer_message == 'yes':
                         self.turn += 1
                         self.middle_row.append((self.turn, chosen_card))
