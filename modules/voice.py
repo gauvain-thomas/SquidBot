3af132c932c3e7ae4942b7a9e803a6e14400e47b
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-03-23 16:21:21 +0100

    Uncomment youtube

--- modules/voice.py
+++ modules/voice.py
@@ -1,61 +1,61 @@
 import discord
 from discord.ext import commands
-# import youtube_dl
-# import json
-# from apiclient.discovery import build
-# from apiclient.errors import HttpError
-# from googleapiclient.discovery import build
-# from googleapiclient.errors import HttpError
-# DEVELOPER_KEY = "AIzaSyC7Zxm1fWNZJOiw_CNIuB5s99x9DxKz0ao"
-# YOUTUBE_API_SERVICE_NAME = "youtube"
-# YOUTUBE_API_VERSION = "v3"
+import youtube_dl
+import json
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+DEVELOPER_KEY = "AIzaSyC7Zxm1fWNZJOiw_CNIuB5s99x9DxKz0ao"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
 
 
-# def youtube_search(q, max_results=5,order="relevance", token=None, location=None, location_radius=None):
+def youtube_search(q, max_results=5,order="relevance", token=None, location=None, location_radius=None):
 
-#   youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-#     developerKey=DEVELOPER_KEY)
+  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+    developerKey=DEVELOPER_KEY)
 
-#   search_response = youtube.search().list(
-#     q=q,
-#     type="video",
-#     pageToken=token,
-#     order = order,
-#     part="id,snippet",
-#     maxResults=max_results,
-#     location=location,
-#     locationRadius=location_radius
+  search_response = youtube.search().list(
+    q=q,
+    type="video",
+    pageToken=token,
+    order = order,
+    part="id,snippet",
+    maxResults=max_results,
+    location=location,
+    locationRadius=location_radius
 
-#   ).execute()
-
-
-
-#   videos = []
-
-#   for search_result in search_response.get("items", []):
-#     if search_result["id"]["kind"] == "youtube#video":
-#       videos.append(search_result)
-#   try:
-#       nexttok = search_response["nextPageToken"]
-#       return(nexttok, videos)
-#   except Exception as e:
-#       nexttok = "last_page"
-#       return(nexttok, videos)
+  ).execute()
 
 
 
-# def grab_video(keyword):
+  videos = []
 
-#     video_dict = {'youID':[], 'title':[]}
-#     result = youtube_search(keyword)
-#     videos = result[1]
+  for search_result in search_response.get("items", []):
+    if search_result["id"]["kind"] == "youtube#video":
+      videos.append(search_result)
+  try:
+      nexttok = search_response["nextPageToken"]
+      return(nexttok, videos)
+  except Exception as e:
+      nexttok = "last_page"
+      return(nexttok, videos)
 
-#     for video in videos:
-#         video_dict['youID'].append(video['id']['videoId'])
-#         video_dict['title'].append(video['snippet']['title'])
-#     return video_dict
 
-# print(grab_video('hollow knight'))
+
+def grab_video(keyword):
+
+    video_dict = {'youID':[], 'title':[]}
+    result = youtube_search(keyword)
+    videos = result[1]
+
+    for video in videos:
+        video_dict['youID'].append(video['id']['videoId'])
+        video_dict['title'].append(video['snippet']['title'])
+    return video_dict
+
+print(grab_video('hollow knight'))
 
 
 
